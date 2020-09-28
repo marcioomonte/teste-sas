76d975b6afc142bfb102a9ad1347cd6010cdeed5
Marcio Monte <marciomonte@proabakus.com.br>
2020-09-28 02:42:25 -0300

    inicia regra de negocio utilizando contexto

--- src/pages/QuestionContent/index.js
+++ src/pages/QuestionContent/index.js
@@ -1,27 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageHeader from '../../components/PageHeader';
 import './styles.css';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import useQuestions from '../../hooks/useQuestions';
 
-import { ReactComponent as StarIcon } from '../../assets/images/star.svg';
+import Modal from '../../components/Modal';
+import Drawer from '../../components/Drawer';
+import Button from '../../components/Button';
+
+import {
+  startTest,
+  initTest,
+  useTestContext,
+  addAnswer,
+} from '../../contexts/testContext';
+
+import RateBalloon from '../../components/RateBalloon';
 
 function QuestionContent() {
   const { state } = useLocation();
-  const [questions] = useQuestions(state.id);
+  const history = useHistory();
+  const [questions, , , getQuestions] = useQuestions();
+
+  const [
+    modalAnswerSelectedVisibility,
+    setModalAnswerrSelectedVisibility,
+  ] = useState(false);
+  const [modalAnswerVisibility, setModalAnswerVisibility] = useState(false);
+  const [referenceTest, setReferenceTest] = useState();
+  const [question, setQuestion] = useState(questions[0]);
+  const [selectedAnswer, setSelectedAnswer] = useState();
+  const [success, setSuccess] = useState(false);
+  const { tests, testDispatcher } = useTestContext();
+
+  useEffect(() => {
+    setModalAnswerVisibility(false);
+    const matchTest = tests?.tests?.find(
+      (test) => test.category === state.name
+    );
+
+    if (matchTest.answers.length > 9) {
+      history.push('/results');
+    }
+    setQuestion(getQuestions(state.id, 'medium'));
+    setReferenceTest(
+      tests?.tests?.find((test) => test.category === state.name)
+    );
+  }, [tests]);
+
+  useEffect(() => {
+    if (referenceTest?.answers.length === 0) {
+      setQuestion(getQuestions(state.id, 'medium'));
+    }
+  }, [referenceTest]);
+
+  const onAnswerSelected = (question, answer) => {
+    setSelectedAnswer(answer);
+    setModalAnswerrSelectedVisibility(true);
+  };
+
+  const handleAnswer = () => {
+    if (questions[0].correct_answer === selectedAnswer) {
+      setModalAnswerrSelectedVisibility(false);
+      setSuccess(true);
+      setModalAnswerVisibility(true);
+    } else {
+      setModalAnswerrSelectedVisibility(false);
+      setSuccess(false);
+      setModalAnswerVisibility(true);
+    }
+  };
+
+  const handleNext = () => {
+    testDispatcher(
+      addAnswer({ ...questions[0], selected_answer: selectedAnswer })
+    );
+  };
 
   return (
     <>
       <PageHeader title={state.name} />
       <div id='question-content-container' className='container'>
         <div className='subtitle'>
-          <span>Questão 1</span>
-          <span className='rate-balloon'>
-            <StarIcon className='.star-filled' />
-            <StarIcon className='.star-filled' />
-            <StarIcon className='star-empty' />
-            <p>Médio</p>
-          </span>
+          <span>Question {referenceTest?.answered_questions + 1} / 10</span>
+          <RateBalloon difficulty={questions[0]?.difficulty} />
         </div>
         <div className='question-container'>
           <p>{questions[0]?.question} </p>
@@ -30,7 +92,7 @@ function QuestionContent() {
           {questions[0]?.answers.map((answer) => (
             <div
               key={answer}
-              onClick={() => console.log({ answer })}
+              onClick={() => onAnswerSelected(questions[0], answer)}
               className='answer-box'
             >
               <p>{answer}</p>
@@ -38,6 +100,16 @@ function QuestionContent() {
           ))}
         </div>
       </div>
+      <Drawer visible={modalAnswerSelectedVisibility}>
+        <Button onClick={() => handleAnswer()} icon='arrow'>
+          Answer
+        </Button>
+      </Drawer>
+      <Modal success={success} visible={modalAnswerVisibility}>
+        <Drawer visible={true}>
+          <Button onClick={() => handleNext()}>Next</Button>
+        </Drawer>
+      </Modal>
     </>
   );
 }
