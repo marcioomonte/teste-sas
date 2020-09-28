aa2ffcbdc97f1a396de7d9064fa75f9c28c5cfe8
Marcio Monte <marciomonte@proabakus.com.br>
2020-09-28 08:48:59 -0300

    muda rota para deploy

--- src/pages/ResultContent/index.js
+++ src/pages/ResultContent/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Button from '../../components/Button';
+import PageHeader from '../../components/PageHeader';
 
 // import { Container } from './styles';
 
@@ -8,6 +9,7 @@ function ResultContent() {
   const history = useHistory();
   return (
     <>
+      <PageHeader title='teste' />
       <h1>Resultado</h1>
       <Button onClick={() => history.push('/')}> Back to start</Button>
     </>
