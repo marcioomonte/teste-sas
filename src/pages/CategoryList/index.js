7b9148927c0679d46e484e40d318aa73ba22871e
Marcio Monte <marciomonte@proabakus.com.br>
2020-09-28 02:46:00 -0300

    inializa utilizacao do contexto na lista de categorias

--- src/pages/CategoryList/index.js
+++ src/pages/CategoryList/index.js
@@ -1,35 +1,58 @@
-import React from "react";
+import React from 'react';
 
-import PageHeader from "../../components/PageHeader";
-import CategoryButton from "../../components/CategoryButton";
-import useDeviceWidth from "../../hooks/useDeviceWidth";
-import useCategories from "../../hooks/useCategories";
-import Modal from "../../components/Modal";
+import PageHeader from '../../components/PageHeader';
+import CategoryButton from '../../components/CategoryButton';
+import useDeviceWidth from '../../hooks/useDeviceWidth';
+import useCategories from '../../hooks/useCategories';
+import { startTest, useTestContext } from '../../contexts/testContext';
 
-import "./styles.css";
+import './styles.css';
+import { useHistory } from 'react-router-dom';
+
 function CategoryList() {
+  const { tests, testDispatcher } = useTestContext();
+
   const [deviceWidth] = useDeviceWidth();
   const [categories] = useCategories();
+  const history = useHistory();
+
+  const handleCategoryButton = (_category) => {
+    const matchTestCategory = tests.tests?.find(
+      (test) => test.category === _category.name
+    );
+
+    testDispatcher(
+      startTest({
+        category: _category.name,
+        answered_questions: matchTestCategory?.answered_questions || 0,
+        answers: matchTestCategory?.answers || [],
+      })
+    );
+    history.push('/questions', _category);
+  };
+
   return (
     <>
-      <div id="page-list-category" className="container">
-        <PageHeader title={deviceWidth < 728 ? "Dev Mobile" : "Dev Web"} />
-
-        <div className="page-subtitle">
+      <PageHeader title={deviceWidth < 728 ? 'Dev Mobile' : 'Dev Web'} />
+      <div id='page-list-category' className='container'>
+        <div className='page-subtitle'>
           <p>Categories</p>
         </div>
 
         <main>
-          <div className="list-category">
+          <div className='list-category'>
             {categories.length !== 0
               ? categories.map((category) => (
-                  <CategoryButton key={category.id} category={category} />
+                  <CategoryButton
+                    onClick={() => handleCategoryButton(category)}
+                    key={category.id}
+                    category={category}
+                  />
                 ))
               : `Sem questoes cadastradas`}
           </div>
         </main>
       </div>
-      <Modal visible={true} />
     </>
   );
 }
