1c51d4f0c568fb1fca22e63db51de4028d2f5403
Marcio Monte <marciomonte@proabakus.com.br>
2020-09-28 02:58:21 -0300

    Updates

---
+++ precache-manifest.b5e824f4a69e0b4b55ecc805170fcdaa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60160dc0f266749b5e062b0fcd1b7385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c2c8a0ec2a15cacff1",
+    "url": "/static/css/main.b3c72da8.chunk.css"
+  },
+  {
+    "revision": "f693f2e3450bbc9f1b61",
+    "url": "/static/js/2.f649f0cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f649f0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c2c8a0ec2a15cacff1",
+    "url": "/static/js/main.496f7c5d.chunk.js"
+  },
+  {
+    "revision": "55a54fe631f7f07e67bb",
+    "url": "/static/js/runtime-main.03c666a2.js"
+  },
+  {
+    "revision": "4c5b82230e17abdf941955d9f45c5796",
+    "url": "/static/media/arrow-right.4c5b8223.svg"
+  },
+  {
+    "revision": "108325c7f7ebdde7ec5bb23a586d1b38",
+    "url": "/static/media/cross.108325c7.svg"
+  },
+  {
+    "revision": "ae46876f6f8e83fd0a1b745f8a290852",
+    "url": "/static/media/star.ae46876f.svg"
+  },
+  {
+    "revision": "c6d133bbfaf862a20f04103585f07d70",
+    "url": "/static/media/tick.c6d133bb.svg"
+  }
+]);
