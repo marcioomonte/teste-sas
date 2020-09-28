349b0edcc60aa591ff7e40d61b6bbd6a965352ea
Marcio Monte <marciomonte@proabakus.com.br>
2020-09-28 13:55:45 -0300

    Updates

---
+++ precache-manifest.cc39cf2d4d8b8c53857db94994ded940.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5826f6ca8d1917b726bc385abe59c1",
+    "url": "/teste-sas/index.html"
+  },
+  {
+    "revision": "516083684eec8ba58d7b",
+    "url": "/teste-sas/static/css/main.1e49a554.chunk.css"
+  },
+  {
+    "revision": "ab726ab41e6ca6274118",
+    "url": "/teste-sas/static/js/2.49619a1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/teste-sas/static/js/2.49619a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516083684eec8ba58d7b",
+    "url": "/teste-sas/static/js/main.1079c4f7.chunk.js"
+  },
+  {
+    "revision": "04ce97bf799ee5f50a73",
+    "url": "/teste-sas/static/js/runtime-main.ed526cf7.js"
+  },
+  {
+    "revision": "4c5b82230e17abdf941955d9f45c5796",
+    "url": "/teste-sas/static/media/arrow-right.4c5b8223.svg"
+  },
+  {
+    "revision": "108325c7f7ebdde7ec5bb23a586d1b38",
+    "url": "/teste-sas/static/media/cross.108325c7.svg"
+  },
+  {
+    "revision": "ae46876f6f8e83fd0a1b745f8a290852",
+    "url": "/teste-sas/static/media/star.ae46876f.svg"
+  },
+  {
+    "revision": "c6d133bbfaf862a20f04103585f07d70",
+    "url": "/teste-sas/static/media/tick.c6d133bb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef5f14210774993336a8872f88564c40.js"
+  "/teste-sas/precache-manifest.cc39cf2d4d8b8c53857db94994ded940.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teste-sas/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
