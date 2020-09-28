bc6a899e4a8be5655862bdc2141259b3e8db9c20
Marcio Monte <marciomonte@proabakus.com.br>
2020-09-28 02:38:03 -0300

    adicionada rota para resultados

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import CategoryList from "./pages/CategoryList";
-import QuestionContent from "./pages/QuestionContent";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CategoryList from './pages/CategoryList';
+import QuestionContent from './pages/QuestionContent';
+import ResultContent from './pages/ResultContent';
 
 function Routes() {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/questions">
+          <Route path='/results'>
+            <ResultContent />
+          </Route>
+          <Route path='/questions'>
             <QuestionContent />
           </Route>
-          <Route path="/">
+          <Route path='/'>
             <CategoryList />
           </Route>
         </Switch>
