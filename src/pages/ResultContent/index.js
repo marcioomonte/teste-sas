edb31206fe089c48ea66e16473170591d5cf8695
Marcio Monte <marciomonte@proabakus.com.br>
2020-09-29 22:08:30 -0300

    component results css

--- src/pages/ResultContent/index.js
+++ src/pages/ResultContent/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import Button from '../../components/Button';
 import PageHeader from '../../components/PageHeader';
-
-// import { Container } from './styles';
+import mascote from '../../assets/images/mascote.png';
+import './styles.css';
 
 function ResultContent() {
   const history = useHistory();
+  const { state } = useLocation();
   return (
     <>
-      <PageHeader title='teste' />
-      <h1>Resultado</h1>
-      <Button onClick={() => history.push('/')}> Back to start</Button>
+      <PageHeader title={state} />
+      <div id='resultContaier' className='container'>
+        <img src={mascote} alt='mascote' />
+        <hr />
+        <div className='balloon'>
+          <h4>Check your performance in the test</h4>
+        </div>
+        <div className='total-container'>
+          <div className='total'>
+            <strong>7</strong>
+            <p>Corrects</p>
+          </div>
+
+          <div className='total'>
+            <strong>7</strong>
+            <p>Incorrects</p>
+          </div>
+        </div>
+
+        <div className='score-container'>
+          <div>
+            <strong>Easy</strong> <p>Correct: 1</p>
+            <p>Incorrect: 2</p>
+          </div>
+          <div>
+            <strong>Medium</strong> <p>Correct: 1</p>
+            <p>Medium: 2</p>
+          </div>
+          <div>
+            <strong>Hard</strong> <p>Correct: 1</p>
+            <p>Incorrect: 2</p>
+          </div>
+        </div>
+        <div className='button-back'>
+          <Button onClick={() => history.push('/')}>Back to start</Button>
+        </div>
+      </div>
     </>
   );
 }
